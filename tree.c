2488bbc53f052c060b32aface26fe3231464f027
cfolmar <cfolma>
2014-03-05 20:54:04 -0500

    new file:   tree.c

---
+++ tree.c
@@ -0,0 +1,24 @@
+/*Binary Tree implementation of Huffman Encoding
+ *We are using the most basic form of huffman encoding
+ *ie. only weighing individual ascii characters by 
+ *occurance and then building the encoding tree directly 
+ */
+
+
+/*Constants*/
+
+
+
+
+
+
+
+
+
+/*local variables*/
+
+void enque(char *que, however we decide to store the characters);
+char *dequeue(void);
+
+
+
