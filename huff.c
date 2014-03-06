ecbe1ca65205de9c8c54eb07790df301ea2772e9
cfolmar <cfolma>
2014-03-05 20:52:08 -0500

    Initial commit

---
+++ huff.c
@@ -0,0 +1,39 @@
+/* A simple Huffman encoding implementation
+ *
+ *          Christopher Folmar
+ *
+ *
+ * Do whatever the fuck you like 
+ *
+ */
+
+
+#include <stdio.h>
+
+
+/*Constants*/
+
+
+
+
+
+
+/*local variables*/
+
+int proc_file(FP *fp,somehting);
+
+
+
+
+
+int main(int argc, char[] argv)
+{
+    
+
+
+
+
+
+
+
+}
